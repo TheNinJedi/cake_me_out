c6d5742e1aae1dcd757eef37df0e36c025b9497c
TheNinJedi <TheNinJedi@gmail.com>
2013-10-14 20:40:15 -0600

    Creates default rails app with postgresql and gitignore changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CakeMeOut::Application.config.secret_token = 'ff39c7e863ec0b6fea9ab8a6ec96ede437d034e4c17efc9ef28610919fabc536094a887692e54bc7c97f30290ac15c2e2c9d2655e882591f4ccf0ba5907a94f0'
